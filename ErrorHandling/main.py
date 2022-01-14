1c57486253d26d3b77ebe9d31bea6b185296c908
ankit bharti <ankitbharti1994@gmail.com>
2022-01-14 12:15:28 +0530

    Error Hanlding

--- ErrorHandling/main.py
+++ ErrorHandling/main.py
@@ -1,12 +1,16 @@
-def divide(num1, num2):
-    return num1 / num2
-
-try:
-    result = divide(5, 1)
-except ZeroDivisionError:
-    print('tried to divide using 0')
-    raise
-else:
-    print(f'result is {result}')
-finally:
-    print('executing finally clause')
+try:  # Try to execute the line of code that may throw an error
+    file = open('abc.txt')
+    user = {"name": "ankit bharti"}
+    print(user["name"])
+except FileNotFoundError as error_message:  # Catch the error that may throw based on the code written in try block
+    print(f'{error_message} not exist. So, creating new one.')
+    file = open('abc.txt', 'w')
+    file.write('something')
+except KeyError as error_message:
+    print(f"{error_message} key doesn't exist.")
+else:  # Perform block of code if there is no error thrown in the try block
+    contents = file.read()
+    print(contents)
+finally:  # Perform the final block of code like close file, no matter what happens in try block
+    file.close()
+    print('file closed')
